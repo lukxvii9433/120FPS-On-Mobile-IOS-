4d1136167c0bb3be2cb3f6b255306c3f1474c788
lukxvii9433 <steefsteef576@gmail.com>
2026-08-12 00:55:20 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,1 @@
-#include <Geode/Geode.hpp>
-#include <Geode/modify/AppDelegate.hpp>
 
-using namespace geode::prelude;
-
-class $modify(FPSUnlockAppDelegate, AppDelegate) {
-    bool applicationDidFinishLaunching() {
-        if (!AppDelegate::applicationDidFinishLaunching()) return false;
-
-        if (auto director = CCDirector::sharedDirector()) {
-            director->setAnimationInterval(1.0 / 120.0);
-        }
-        return true;
-    }
-};
-
-$execute {
-    log::info("120 FPS Unlock loaded");
-}
